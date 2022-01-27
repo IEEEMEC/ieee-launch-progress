73b81e6c627c45b1f9842d6b7d4ef72727747652
Bharath Srinivasan <bharath.s@ieee.org>
2022-01-27 14:12:48 +0530

    minor fixes

--- index.js
+++ index.js
@@ -3,13 +3,7 @@ let button=document.getElementById('launch')
 let bar=document.getElementById('bar')
 
   
-button.onclick=()=>{var xhr = typeof XMLHttpRequest != 'undefined' ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-xhr.open('get', '/file.html', true);
-xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && xhr.status == 200) { 
-        bar.innerHTML = xhr.responseText;
-    } 
-}
-xhr.send();
-setTimeout(()=>{location.href="//codewithharry.com"},3000)
+button.onclick=()=>{
+        bar.innerHTML = '<div class="progress"><div class="progress__bar"></div></div>';
+setTimeout(()=>{location.href="https://www.ieeemec.org"},3000)
 }
